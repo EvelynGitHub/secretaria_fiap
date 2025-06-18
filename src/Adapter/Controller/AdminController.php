7f67a0cc42c94dd71611f9147b24cd27fc5af2ce
Evelyn <evelynbrandao15@gmail.com>
2025-06-18 11:20:58 -0300

    HttpOnly ajustado para funcionar em cross-site

--- src/Adapter/Controller/AdminController.php
+++ src/Adapter/Controller/AdminController.php
@@ -30,30 +30,36 @@ class AdminController
 
             $output = $this->casoUsoLogin->executar($input);
 
-            // JWT HttpOnly
-            $response = $response->withHeader('Set-Cookie', 'auth_token=' . $output->token . '; Path=/; HttpOnly; SameSite=Lax');
+            // JWT HttpOnly (forma correta de usar o response do slim)
+            $response = $response
+                ->withHeader(
+                    'Set-Cookie',
+                    "auth_token={$output->token}; Path=/; HttpOnly; SameSite=None; Secure"
+                )
+                ->withHeader('Content-Type', 'application/json')
+                ->withStatus(200);
 
             // Para poder testar pelo swagger também
             $response->getBody()->write(json_encode($output));
-            $response->withStatus(200);
-            return $response;//->withJson(['success' => true, 'message' => 'Login bem-sucedido!']);
+
+            return $response;
 
             // JWT HttpOnly - EXPLICAÇÃO DAS LINHAS
             // Configura o cookie 'auth_token' com o token JWT
             // Path=/      : O cookie estará disponível para todo o domínio
             // HttpOnly    : O cookie não pode ser acessado via JavaScript (segurança XSS)
-            // Secure      : O cookie SÓ será enviado se a conexão for HTTPS (ESSENCIAL PARA PRODUÇÃO) (REMOVIDO POR NÃO SER PRODUÇÃO)
-            // SameSite=Lax: Protege contra CSRF (pode ser 'Strict' para mais segurança, mas pode impactar links externos)
-            // $response = $response->withHeader('Set-Cookie', 'auth_token=' . $output->token . '; Path=/; HttpOnly; Secure; SameSite=Lax');
-            // Retorna a resposta de sucesso, sem o token no corpo do JSON, pois ele está no cookie
-            // O corpo da resposta pode ser vazio ou conter apenas uma mensagem de sucesso
+            // Secure      : O cookie SÓ será enviado se a conexão for HTTPS (ESSENCIAL PARA PRODUÇÃO)
+            //     - Secure é obrigatório com SameSite=None
+            // SameSite: Protege contra CSRF (pode ser 'Strict' para mais segurança, mas pode impactar links externos)
+            //     - SameSite=Lax: impede que o cookie seja enviado em requisições cross-origin (ex: frontend em porta/domínio, diferente do backend).
+            //     - SameSite=None: permite envio de cookie entre origens (cross-site).
         } catch (\Throwable $th) {
             return HttpResposta::erro($th);
         }
     }
 
     /**
-     * Logout simplesmente invalidar o token JWT jogando numa blacklist.
+     * Logout simplesmente invalidar o token JWT .
      * Isso faz com que o token JWT atual não seja mais válido, forçando o usuário a fazer login novamente.
      * Isso é uma abordagem simples para logout, mesmo assim, não é recomendada para produção.
      * @param \Psr\Http\Message\ServerRequestInterface $request
@@ -82,13 +88,16 @@ class AdminController
 
             // Comentado devido ao HttpOnly estar funcionando (e isso tem problemas de permissão)
             // TokenHelper::adicionarTokenBlacklist($jwt);
-
-            // Invalida o cookie 'auth_token' definindo sua data de expiração no passado
+            // Invalida o cookie 'auth_token' definindo sua data de expiração no passado e deleta o token com auth_token=deleted;
             // As flags (Path, HttpOnly, Secure, SameSite) devem ser as mesmas do cookie original
-            $response = $response->withHeader('Set-Cookie', 'auth_token=; Path=/; HttpOnly; Expires=Thu, 01 Jan 1970 00:00:00 GMT; SameSite=Lax');
+            // Max-Age=0 apaga o cookie de forma universal.
 
-            $response->getBody()->write(json_encode(["mensagem" => "Logout efetuado com sucesso."]));
-            $response->withStatus(200);
+            $response = $response
+                ->withHeader('Set-Cookie', 'auth_token=deleted; Path=/; HttpOnly; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Max-Age=0; SameSite=None; Secure')
+                ->withHeader('Content-Type', 'application/json')
+                ->withStatus(200);
+
+            $response->getBody()->write(json_encode(['mensagem' => 'Logout com sucesso']));
             return $response;
         } catch (\Throwable $th) {
             return HttpResposta::erro($th);
